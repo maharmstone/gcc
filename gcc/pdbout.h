817956f494b3bacc55c45ebbadcc21a36f509957
Mark Harmstone <mark@harmstone.com>
2020-04-23 18:26:13 +0100

    add .pdb section

--- gcc/pdbout.h
+++ gcc/pdbout.h
@@ -1,11 +1,5 @@
 #ifndef GCC_PDBOUT_H
 #define GCC_PDBOUT_H 1
 
-extern void pdbout_begin_prologue (unsigned int line ATTRIBUTE_UNUSED,
-				   unsigned int column ATTRIBUTE_UNUSED,
-				   const char *file ATTRIBUTE_UNUSED);
-
-extern void pdbout_end_epilogue (unsigned int line ATTRIBUTE_UNUSED,
-				 const char *file ATTRIBUTE_UNUSED);
 
 #endif
