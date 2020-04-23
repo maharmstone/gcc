5ee814add3ff7efe3f6dbd2b2d3bdb8498d49b05
Mark Harmstone <mark@harmstone.com>
2020-04-24 00:28:34 +0100

    list funcs in pdb section

--- gcc/pdbout.c
+++ gcc/pdbout.c
@@ -8,6 +8,7 @@
 #include "output.h"
 #include "target.h"
 #include "defaults.h"
+#include "print-tree.h"
 
 #define FUNC_BEGIN_LABEL	"LFB"
 #define FUNC_END_LABEL		"LFE"
@@ -20,6 +21,10 @@ static void pdbout_end_epilogue (unsigned int line ATTRIBUTE_UNUSED,
 				 const char *file ATTRIBUTE_UNUSED);
 
 static void pdbout_finish (const char *filename);
+
+static void pdbout_begin_function(tree func);
+
+static struct pdb_func *funcs = NULL;
 
 const struct gcc_debug_hooks pdb_debug_hooks =
 {
@@ -39,7 +44,7 @@ const struct gcc_debug_hooks pdb_debug_hooks =
   debug_nothing_int_charstar,	         /* end_prologue */
   debug_nothing_int_charstar,	         /* begin_epilogue */
   pdbout_end_epilogue,		         /* end_epilogue */
-  debug_nothing_tree,		         /* begin_function */
+  pdbout_begin_function,	         /* begin_function */
   debug_nothing_int,		         /* end_function */
   debug_nothing_tree,		         /* register_main_translation_unit */
   debug_nothing_tree,		         /* function_decl */
@@ -62,7 +67,7 @@ const struct gcc_debug_hooks pdb_debug_hooks =
   TYPE_SYMTAB_IS_ADDRESS                 /* tree_type_symtab_field */
 };
 
-void
+static void
 pdbout_begin_prologue (unsigned int line ATTRIBUTE_UNUSED,
 		       unsigned int column ATTRIBUTE_UNUSED,
 		       const char *file ATTRIBUTE_UNUSED)
@@ -71,7 +76,7 @@ pdbout_begin_prologue (unsigned int line ATTRIBUTE_UNUSED,
 			  current_function_funcdef_no);
 }
 
-void
+static void
 pdbout_end_epilogue (unsigned int line ATTRIBUTE_UNUSED,
 		     const char *file ATTRIBUTE_UNUSED)
 {
@@ -79,10 +84,73 @@ pdbout_end_epilogue (unsigned int line ATTRIBUTE_UNUSED,
 			  current_function_funcdef_no);
 }
 
-void
-pdbout_finish (const char *filename)
+static void
+pdbout_lproc32 (struct pdb_func *func)
+{
+  size_t name_len = strlen (func->name);
+
+  // start procedure
+
+  fprintf (asm_out_file, "\t.balign\t4\n");
+
+  ASM_OUTPUT_DEBUG_LABEL (asm_out_file, "cvprocstart", func->num);
+
+  fprintf (asm_out_file, "\t.short\t0x%lx\n", 37 + name_len + 1); // reclen
+  fprintf (asm_out_file, "\t.short\t0x%x\n", CODEVIEW_S_LPROC32); // rectyp (FIXME - GPROC32 if appropriate)
+  fprintf (asm_out_file, "\t.long\t0\n"); // pParent
+  fprintf (asm_out_file, "\t.long\t[cvprocend%u]-[cvprocstart%u]\n", func->num, func->num); // pEnd
+  fprintf (asm_out_file, "\t.long\t0\n"); // pNext
+  fprintf (asm_out_file, "\t.long\t[" FUNC_END_LABEL "%u]-[" FUNC_BEGIN_LABEL "%u]\n", func->num, func->num); // len
+  fprintf (asm_out_file, "\t.long\t0\n"); // FIXME - DbgStart
+  fprintf (asm_out_file, "\t.long\t0\n"); // FIXME - DbgEnd
+  fprintf (asm_out_file, "\t.long\t0\n"); // FIXME - typeind
+  fprintf (asm_out_file, "\t.long\t[" FUNC_BEGIN_LABEL "%u]\n", func->num); // off
+  fprintf (asm_out_file, "\t.short\t0\n"); // seg (will get set by the linker)
+  fprintf (asm_out_file, "\t.byte\t0\n"); // FIXME - flags
+  ASM_OUTPUT_ASCII (asm_out_file, func->name, strlen (func->name) + 1);
+
+  // FIXME - S_FRAMEPROC, S_BPREL32, S_CALLSITEINFO, etc.
+
+  // end procedure
+
+  fprintf (asm_out_file, "\t.balign\t4\n");
+
+  ASM_OUTPUT_DEBUG_LABEL (asm_out_file, "cvprocend", func->num);
+
+  fprintf (asm_out_file, "\t.short\t0x2\n");
+  fprintf (asm_out_file, "\t.short\t0x%x\n", CODEVIEW_S_END);
+}
+
+static void
+pdbout_finish (const char *filename ATTRIBUTE_UNUSED)
 {
   fprintf (asm_out_file, "\t.section\t.pdb, \"ndr\"\n");
 
-  // FIXME
+  while (funcs) {
+    struct pdb_func *n;
+
+    pdbout_lproc32(funcs);
+
+    n = funcs->next;
+
+    if (funcs->name)
+      free (funcs->name);
+
+    free(funcs);
+
+    funcs = n;
+  }
+
+}
+
+static void
+pdbout_begin_function (tree func)
+{
+  struct pdb_func *f = (struct pdb_func*)xmalloc(sizeof(struct pdb_func));
+
+  f->next = funcs;
+  f->name = xstrdup(IDENTIFIER_POINTER(DECL_NAME(func)));
+  f->num = current_function_funcdef_no;
+
+  funcs = f;
 }
