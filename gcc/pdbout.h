79918c4b6ccae2161e241582ec4a9314b00bc6bb
Mark Harmstone <mark@harmstone.com>
2020-04-23 04:09:26 +0100

    add pdbout_begin_prologue

---
+++ gcc/pdbout.h
@@ -0,0 +1,8 @@
+#ifndef GCC_PDBOUT_H
+#define GCC_PDBOUT_H 1
+
+extern void pdbout_begin_prologue (unsigned int line ATTRIBUTE_UNUSED,
+				   unsigned int column ATTRIBUTE_UNUSED,
+				   const char *file ATTRIBUTE_UNUSED);
+
+#endif
