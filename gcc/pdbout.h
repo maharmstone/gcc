f4d26e1feb44d93efa54890c9fea76fdfa95e162
Mark Harmstone <mark@harmstone.com>
2020-04-24 00:28:34 +0100

    list funcs in pdb section

--- gcc/pdbout.h
+++ gcc/pdbout.h
@@ -1,5 +1,13 @@
 #ifndef GCC_PDBOUT_H
 #define GCC_PDBOUT_H 1
 
+#define CODEVIEW_S_END		0x0006 // procedure end
+#define CODEVIEW_S_LPROC32	0x110f // local procedure start
+
+struct pdb_func {
+  struct pdb_func *next;
+  char *name;
+  int num;
+};
 
 #endif
