a4d4258c33d9acc872f48e464f5bfe0733f1b123
Mark Harmstone <mark@harmstone.com>
2020-04-25 21:40:45 +0100

    use S_GPROC32 rather than S_LPROC32 for non-static functions

--- gcc/pdbout.h
+++ gcc/pdbout.h
@@ -1,8 +1,9 @@
 #ifndef GCC_PDBOUT_H
 #define GCC_PDBOUT_H 1
 
-#define CODEVIEW_S_END		0x0006 // procedure end
-#define CODEVIEW_S_LPROC32	0x110f // local procedure start
+#define CODEVIEW_S_END		0x0006
+#define CODEVIEW_S_GPROC32	0x1110
+#define CODEVIEW_S_LPROC32	0x110f
 
 #define CV_SIGNATURE_C13	4
 
@@ -10,6 +11,7 @@ struct pdb_func {
   struct pdb_func *next;
   char *name;
   int num;
+  unsigned int public_flag;
 };
 
 #endif
