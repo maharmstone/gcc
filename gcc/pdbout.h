dbb048be5af20280b0c3ac2c53cd294bb9bd6881
Mark Harmstone <mark@harmstone.com>
2020-04-26 17:05:06 +0100

    record fieldlist entries for struct types

--- gcc/pdbout.h
+++ gcc/pdbout.h
@@ -6,6 +6,8 @@
 #define CODEVIEW_S_GDATA32	0x110d
 #define CODEVIEW_S_LPROC32	0x110f
 #define CODEVIEW_S_GPROC32	0x1110
+#define CODEVIEW_LF_FIELDLIST	0x1203
+#define CODEVIEW_LF_MEMBER	0x150d
 
 #define CV_SIGNATURE_C13	4
 
@@ -21,6 +23,38 @@ struct pdb_global_var {
   char *name;
   char *asm_name;
   unsigned int public_flag;
+};
+
+struct pdb_struct_fieldlist_entry {
+  uint32_t offset;
+  uint16_t type;
+  char *name;
+};
+
+struct pdb_struct_fieldlist {
+  uint16_t num;
+  unsigned int count;
+  struct pdb_struct_fieldlist_entry entries[1];
+};
+
+struct pdb_fieldlist_entry {
+  uint16_t cv_type;
+  uint16_t type;
+  uint16_t offset;
+  char* name;
+};
+
+struct pdb_fieldlist {
+  unsigned int count;
+  struct pdb_fieldlist_entry *entries;
+};
+
+struct pdb_type {
+  struct pdb_type *next;
+  uint16_t id;
+  tree_node *tree;
+  uint16_t cv_type;
+  uint8_t data[1];
 };
 
 #endif
